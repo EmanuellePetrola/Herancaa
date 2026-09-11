2b345e04cd0e601afbdaa4a76d966be9dba0f64f
EmanuellePetrola <emanuelle.petrola@edu.senai.br>
2026-09-11 12:04:17 -0300

    Herancaa

---
+++ Guerreiro.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Herancaa
+    {
+        class Guerreiro : Personagem
+        {
+            public int Defesa { get; set; }
+
+            public Guerreiro(string nome, int pontosDeVida, int forcaDeAtaque, int defesa)
+                : base(nome, pontosDeVida, forcaDeAtaque)
+            {
+                Defesa = defesa;
+            }
+
+            public override void Atacar(Personagem alvo)
+            {
+                int dano = (int)(ForcaDeAtaque * 1.2);
+
+                alvo.PontosDeVida -= dano;
+
+                if (alvo.PontosDeVida < 0)
+                {
+                    alvo.PontosDeVida = 0;
+                }
+
+                Console.WriteLine($"{Nome} causou {dano} de dano em {alvo.Nome}.");
+                Console.WriteLine($"{alvo.Nome} ficou com {alvo.PontosDeVida} PV.");
+            }
+        }
+    }
+
---
+++ Mago.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Herancaa
+{
+        class Mago : Personagem
+        {
+            public int PontosDeMagia { get; set; }
+
+            public Mago(string nome, int pontosDeVida, int forcaDeAtaque, int pontosDeMagia)
+                : base(nome, pontosDeVida, forcaDeAtaque)
+            {
+                PontosDeMagia = pontosDeMagia;
+            }
+
+            public override void Atacar(Personagem alvo)
+            {
+                int dano = ForcaDeAtaque;
+
+                alvo.PontosDeVida -= dano;
+
+                if (alvo.PontosDeVida < 0)
+                {
+                    alvo.PontosDeVida = 0;
+                }
+
+                int recuperacao = (int)(dano * 0.1);
+                PontosDeMagia += recuperacao;
+
+                Console.WriteLine($"{Nome} causou {dano} de dano em {alvo.Nome}.");
+                Console.WriteLine($"{alvo.Nome} ficou com {alvo.PontosDeVida} PV.");
+                Console.WriteLine($"{Nome} recuperou {recuperacao} MP.");
+            }
+        }
+    }
+
